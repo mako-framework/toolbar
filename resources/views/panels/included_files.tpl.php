463d86d8c751b9e423c6f334fc0d2ad454290cd7
Frederic G. Østby <frederic.g.ostby@gmail.com>
2016-12-23 15:41:36 +0100

    Added more info

--- resources/views/panels/included_files.tpl.php
+++ resources/views/panels/included_files.tpl.php
@@ -6,7 +6,9 @@
 
 	<p><span class="mako-subtitle">Included files:</span></p>
 
-	<p><small>* The file list might not be a 100% accurate as it only contains the files included at the time of panel rendering.</small></p>
+	<p>
+		<small>* The file list might not be a 100% accurate as it only contains the files included at the time of panel rendering. Also keep in mind that some of the files are included by the toolbar and will not be loaded in a production environment.</small>
+	</p>
 
 	<table class="mako-table mako-table-2c">
 		<tr>
