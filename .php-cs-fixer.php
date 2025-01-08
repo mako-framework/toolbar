c6ad1e651f94945d66b41da2da3468d924ddb3f8
Frederic G. Østby <frederic.g.ostby@gmail.com>
2025-01-08 18:44:58 +0100

    Update .php-cs-fixer.php

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -21,6 +21,8 @@ return (new Config)->setRules
 	'combine_nested_dirname' => true,
 	'concat_space' => ['spacing' => 'one'],
 	'constant_case' => true,
+	'control_structure_braces' => true,
+	'control_structure_continuation_position' => ['position' => 'next_line'],
 	'declare_equal_normalize' => true,
 	'declare_parentheses' => true,
 	'dir_constant' => true,
